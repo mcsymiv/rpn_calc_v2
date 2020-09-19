5d75bcf770763068a6f20bc90b36bf452ae52109
mcsymiv <45435157+mcsymiv@users.noreply.github.com>
2020-09-19 11:56:35 +0300

    Add GetPriority method

--- rpn_calc/Rpn.cs
+++ rpn_calc/Rpn.cs
@@ -8,6 +8,19 @@ namespace rpn_calc
 {
     public class Rpn
     {
-
+        public int GetPriority(char s)
+        {
+            switch (s)
+            {
+                case '(': return 0;
+                case ')': return 1;
+                case '+': return 2;
+                case '-': return 3;
+                case '*': return 4;
+                case '/': return 4;
+                case '^': return 5;
+                default: return 6;
+            }
+        }
     }
 }
