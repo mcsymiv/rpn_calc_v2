a8fb236a9b4617ea103dce60cb0d3de3295c37e7
mcsymiv <45435157+mcsymiv@users.noreply.github.com>
2020-09-19 16:08:35 +0300

    Add Nunit framework to the project

---
+++ RPN_UnitTest/Rpn_Test.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using rpn_calc;
+using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RPN_UnitTest
+{
+    [TestFixture]
+    public class Rpn_Test
+    {
+        [TestCase('+')]
+        [TestCase('-')]
+        [TestCase('*')]
+        [TestCase('/')]
+        [TestCase('^')]
+        public void TrueIfValidOperator(char oper)
+        {
+            bool isExpectedOperator = new Rpn().IsOperator(oper);
+            Assert.IsTrue(isExpectedOperator);
+        }
+    }
+}
